aa00317d7ba13c6124b567c0a2cccb67e235337d
Sergey Belookiy <belooki@yandex.ru>
2025-04-01 20:18:32 +0300

    Написал код

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,38 @@
 // @todo: Темплейт карточки
 
-// @todo: DOM узлы
+const cardTemplate = document.querySelector('#card-template').content;
+
+/*// @todo: DOM узлы
+const cloneElement = cardTemplate.querySelector('.card');*/
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(cardInit, deleteFunction) {
+
+    const cardElement = cardTemplate.cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = cardInit.link;
+    cardElement.querySelector('.card__title').textContent = cardInit.name;
+
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+
+    deleteButton.addEventListener ('click', deleteFunction);
+
+    return cardElement;
+};
 
 // @todo: Функция удаления карточки
+function deleteCard(event) {
+    event.target.closest('.card').remove();
+};
 
 // @todo: Вывести карточки на страницу
+
+
+initialCards.forEach(cardData => {
+
+    const cardElement = createCard(cardData, deleteCard);
+    
+    placesList.append(cardElement); 
+    console.log(cardElement);
+});
